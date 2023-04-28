f1f3d9f76eb9233f7a2e43f21f3373bfad2aba9f
skawale369 <santoshkawale369@gmail.com>
2023-04-28 12:00:37 +0530

    Basic programme

---
+++ src/test/java/FirstGet.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class FirstGet {
+	
+	//https://reqres.in/api/users/2
+	@Test
+	void testcase01()
+	{
+		
+		Response res=RestAssured.get("https://reqres.in/api/users/2");
+		System.out.println(res.asString());
+		System.out.println("status code:" +res.getStatusCode());
+		
+		
+	}
+
+}
---
+++ src/test/java/session02/GetMethod.java
@@ -0,0 +1,5 @@
+package session02;
+
+public class GetMethod {
+
+}
