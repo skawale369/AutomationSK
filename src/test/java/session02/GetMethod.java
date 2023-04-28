e2e7037a1acc54210e8e8c12385600c4d00beb99
skawale369 <santoshkawale369@gmail.com>
2023-04-28 12:08:12 +0530

    Validation

--- src/test/java/session02/GetMethod.java
+++ src/test/java/session02/GetMethod.java
@@ -1,5 +1,10 @@
 package session02;
 
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 public class GetMethod {
 	
 	
@@ -7,5 +12,16 @@ public class GetMethod {
 	// print response code, response header, response time
 	
 	
+	//https://reqres.in/api/users/2
+		@Test
+		void testcase01()
+		{
+			
+			Response res=RestAssured.get("https://reqres.in/api/users/2");
+			System.out.println(res.asString());
+			System.out.println("status code:" +res.getStatusCode());
+			
+			
+		}
 
 }
